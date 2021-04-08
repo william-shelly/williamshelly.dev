aebbd6f2c89460de5ac824841bbf624a85c7a5f2
william-shelly <william-shelly@users.noreply.github.com>
2021-04-08 13:16:03 -0500

    hiding links

--- src/components/mainnav.js
+++ src/components/mainnav.js
@@ -4,12 +4,12 @@ import { Link } from "gatsby"
 export default function MainNav() {
     return (
         <ul className="main-nav text-center text-lg-left">
-            <li><Link to="/" className="">Home</Link></li>
+            {/* <li><Link to="/" className="">Home</Link></li>
             <li><Link to="/about" className="">About</Link></li>
             <li><a href="/portfolio" className="">Portfolio</a></li>
             <li><a href="/cheat-sheets" className="">Cheat Sheets</a></li>
             <li><a href="/links" className="">Links</a></li>
-            <li><a href="/contact" className="">Contact</a></li>
+            <li><a href="/contact" className="">Contact</a></li> */}
         </ul>
     )
 }
