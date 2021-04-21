00f9735282684f8a033d3537b2ca25a872dfd130
william-shelly <william-shelly@users.noreply.github.com>
2021-04-20 22:38:24 -0500

    Modal bootstrap funcational

--- src/components/portfolio-preview.js
+++ src/components/portfolio-preview.js
@@ -1,31 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { Link } from 'gatsby'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 export default function PortfolioPreview({node}) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <>
       <div className="col-12 mb-4">
         <div className="row">
-          {node.logo.file.url ? <div className="col-12 col-lg-6"><img src={node.logo.file.url} alt={node.title} loading="lazy" width={node.logoWidth} /></div> : undefined }
-          <div className="menu-item-description col-12 col-lg-6 text-center text-lg-left">
+          <div className="menu-item-description col-12 col-lg-6 text-center d-flex justify-content-center align-items-center">
             <h2>{node.title}</h2>
-            {node.featuredImage ? <img src={node.featuredImage.file.url} alt={node.featuredImage.description} loading="lazy" /> : undefined }
+
+            {node.logo.file.url ? <img src={node.logo.file.url} className="d-flex" alt={node.title} loading="lazy" width={node.logoWidth} /> : undefined }
+
+            {node.featuredImage ? <img src={node.featuredImage.file.url} className="d-flex w-100 my-2" alt={node.featuredImage.description} loading="lazy" /> : undefined }
 
             {node.featuredImage ? <p><span dangerouslySetInnerHTML={{ __html: node.featuredImage.description}}></span></p> : undefined }
-
+{/* 
             {node.image02 ? <img src={node.image02.file.url} alt={node.image02.description} loading="lazy" /> : undefined }
 
             {node.image03 ? <img src={node.image03.file.url} alt={node.image03.description} loading="lazy" /> : undefined }
 
-            {node.image04 ? <img src={node.image04.file.url} alt={node.image04.description} loading="lazy" /> : undefined }
+            {node.image04 ? <img src={node.image04.file.url} alt={node.image04.description} loading="lazy" /> : undefined } */}
 
-            <Link className="btn btn-primarycolor d-sm-inline-block text-white" to={"/portfolio/" + node.id}>View {node.title}</Link>
+            {/* <Link className="btn btn-primarycolor d-sm-inline-block text-white" to={"/portfolio/" + node.id}>View {node.title}</Link> */}
 
-            {node.link ? <a className="btn btn-primarycolor d-sm-inline-block text-white mt-2" href={node.link} target="_blank" rel="noreferrer">View {node.title}</a> : undefined }
+            <Button variant="primary" onClick={handleShow}>
+              View {node.title}
+            </Button>
 
+            {/* {node.link ? <a className="btn btn-primarycolor d-sm-inline-block text-white mt-2" href={node.link} target="_blank" rel="noreferrer">View {node.title}</a> : undefined } */}
           </div>
         </div>
       </div>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={true}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title><h2>{node.title}</h2></Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {/* {node.featuredImage ? <img src={node.featuredImage.file.url} alt={node.featuredImage.description} className="w-full" loading="lazy" /> : undefined } */}
+
+          {node.featuredImage ? <img src={node.featuredImage.file.url} className="w-100 my-2" alt={node.featuredImage.description} loading="lazy" /> : undefined }
+
+          {node.featuredImage ? <p><span dangerouslySetInnerHTML={{ __html: node.featuredImage.description}}></span></p> : undefined }
+
+          {node.image02 ? <img src={node.image02.file.url} className="w-100 my-2" alt={node.image02.description} className="w-full" loading="lazy" /> : undefined }
+
+          {node.image03 ? <img src={node.image03.file.url} className="w-100 my-2" alt={node.image03.description} className="w-full" loading="lazy" /> : undefined }
+
+          {node.image04 ? <img src={node.image04.file.url} className="w-100 my-2" alt={node.image04.description} className="w-full" loading="lazy" /> : undefined }
+
+          {/* <Button variant="primary" onClick={handleShow}>
+            View {node.title}
+          </Button>
+
+          {node.link ? <a className="btn btn-primarycolor d-sm-inline-block text-white mt-2" href={node.link} target="_blank" rel="noreferrer">Go to Website</a> : undefined } */}
+        </Modal.Body>
+        <Modal.Footer>
+        </Modal.Footer>
+      </Modal>
     </>
   )
 }
--- src/templates/portfolio.js
+++ src/templates/portfolio.js
@@ -13,10 +13,9 @@ export default function PortfolioPage({pageContext}) {
 						<h1>{pageContent.name}</h1>
 						{/* <p><span dangerouslySetInnerHTML={{ __html: pageContent.description.description}}></span></p> */}
 
+						{pageContent.featuredImage ? <p><span dangerouslySetInnerHTML={{ __html: pageContent.featuredImage.description}}></span></p> : undefined }
 
 						{pageContent.featuredImage ? <img src={pageContent.featuredImage.file.url} alt={pageContent.featuredImage.description} loading="lazy" /> : undefined }
-
-            {pageContent.featuredImage ? <p><span dangerouslySetInnerHTML={{ __html: pageContent.featuredImage.description}}></span></p> : undefined }
 
             {pageContent.image02 ? <img src={pageContent.image02.file.url} alt={pageContent.image02.description} loading="lazy" /> : undefined }
 
