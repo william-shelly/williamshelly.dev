05b32d373334162cb41339fac9cfa55c2e3ae375
william-shelly <william-shelly@users.noreply.github.com>
2021-04-22 14:23:33 -0500

    take 2 moving modal to external file. not working

--- src/components/portfolio-preview.js
+++ src/components/portfolio-preview.js
@@ -1,13 +1,12 @@
 import React, { useState } from "react";  // { useEffect, }
-// import { Link } from 'gatsby'
-import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
-// import PortfolioModal from '../components/portfolio-modal.js'
+import PortfolioModal from '../components/portfolio-modal.js'
 
-export default function PortfolioPreview({node}) {
+
+const PortfolioPreview = ({node}) => {
+
   const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
+  const [work, setWork] = useState({});
   const handleShow = () => setShow(true);
 
   return (
@@ -20,61 +19,25 @@ export default function PortfolioPreview({node}) {
         {node.featuredImage ? <img src={node.featuredImage.file.url} className="d-flex w-100 my-2" alt={node.featuredImage.description} loading="lazy" /> : undefined }
 
         {node.featuredImage ? <p><span dangerouslySetInnerHTML={{ __html: node.featuredImage.description}}></span></p> : undefined }
-{/* 
-        {node.image02 ? <img src={node.image02.file.url} alt={node.image02.description} loading="lazy" /> : undefined }
 
-        {node.image03 ? <img src={node.image03.file.url} alt={node.image03.description} loading="lazy" /> : undefined }
-
-        {node.image04 ? <img src={node.image04.file.url} alt={node.image04.description} loading="lazy" /> : undefined } */}
-
-        {/* <Link className="btn btn-primarycolor d-sm-inline-block text-white" to={"/portfolio/" + node.id}>View {node.title}</Link> */}
-
-        <Button variant="primary my-2" onClick={handleShow}>
+        <Button
+          variant="primary my-2"
+            onClick={ () => {
+              setWork({ node });
+              console.log(node);
+              console.log(work);
+              handleShow();
+          }}
+        >
           View {node.title}
         </Button>
 
-        {/* {node.link ? <a className="btn btn-primarycolor d-sm-inline-block text-white mt-2" href={node.link} target="_blank" rel="noreferrer">View {node.title}</a> : undefined } */}
+        { work.node ? <PortfolioModal node={node} /> : null }
+
       </div>
 
-      {/* <PortfolioModal
-        node={node}
-        show={show}
-        onHide={handleClose}
-        backdrop="static"
-        keyboard={true}>
-      </PortfolioModal> */}
-
-      <Modal
-        show={show}
-        onHide={handleClose}
-        backdrop="static"
-        keyboard={true}
-      >
-        <Modal.Header closeButton>
-          <Modal.Title><h2>{node.title}</h2></Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          {/* {node.featuredImage ? <img src={node.featuredImage.file.url} alt={node.featuredImage.description} className="w-full" loading="lazy" /> : undefined } */}
-
-          {node.featuredImage ? <img src={node.featuredImage.file.url} className="w-100 my-2" alt={node.featuredImage.description} loading="lazy" /> : undefined }
-
-          {node.featuredImage ? <p><span dangerouslySetInnerHTML={{ __html: node.featuredImage.description}}></span></p> : undefined }
-
-          {node.image02 ? <img src={node.image02.file.url} className="w-100 my-2" alt={node.image02.description} className="w-full" loading="lazy" /> : undefined }
-
-          {node.image03 ? <img src={node.image03.file.url} className="w-100 my-2" alt={node.image03.description} className="w-full" loading="lazy" /> : undefined }
-
-          {node.image04 ? <img src={node.image04.file.url} className="w-100 my-2" alt={node.image04.description} className="w-full" loading="lazy" /> : undefined }
-
-          {/* <Button variant="primary" onClick={handleShow}>
-            View {node.title}
-          </Button>
-
-          {node.link ? <a className="btn btn-primarycolor d-sm-inline-block text-white mt-2" href={node.link} target="_blank" rel="noreferrer">Go to Website</a> : undefined } */}
-        </Modal.Body>
-        <Modal.Footer>
-        </Modal.Footer>
-      </Modal>
     </>
   )
 }
+
+export default PortfolioPreview
