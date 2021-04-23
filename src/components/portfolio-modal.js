4b481fcc8c780ffd2da8654fe94e6eff68fe28fc
william-shelly <william-shelly@users.noreply.github.com>
2021-04-22 20:13:27 -0500

    removing unused styles

--- src/components/portfolio-modal.js
+++ src/components/portfolio-modal.js
@@ -18,23 +18,10 @@ export default function PortfolioModal({node}) {
 				<Modal.Title><h2>{node.title}</h2></Modal.Title>
 			</Modal.Header>
 			<Modal.Body>
-				{/* {node.featuredImage ? <img src={node.featuredImage.file.url} alt={node.featuredImage.description} className="w-full" loading="lazy" /> : undefined } */}
-
 				{node.featuredImage ? <img src={node.featuredImage.file.url} className="w-100 my-2" alt={node.featuredImage.description} loading="lazy" /> : undefined }
 
 				{node.featuredImage ? <p><span dangerouslySetInnerHTML={{ __html: node.featuredImage.description}}></span></p> : undefined }
 
-				{/* {node.image02 ? <img src={node.image02.file.url} className="w-100 my-2" alt={node.image02.description} className="w-full" loading="lazy" /> : undefined }
-
-				{node.image03 ? <img src={node.image03.file.url} className="w-100 my-2" alt={node.image03.description} className="w-full" loading="lazy" /> : undefined }
-
-				{node.image04 ? <img src={node.image04.file.url} className="w-100 my-2" alt={node.image04.description} className="w-full" loading="lazy" /> : undefined } */}
-
-				{/* <Button variant="primary" onClick={handleShow}>
-					View {node.title}
-				</Button>
-
-				{node.link ? <a className="btn btn-primarycolor d-sm-inline-block text-white mt-2" href={node.link} target="_blank" rel="noreferrer">Go to Website</a> : undefined } */}
 			</Modal.Body>
 			<Modal.Footer>
 			</Modal.Footer>
