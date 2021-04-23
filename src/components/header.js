5e8f935f1c53ab6feee870679d37b7eb4204c07b
william-shelly <william-shelly@users.noreply.github.com>
2021-04-23 11:05:58 -0500

    Adding mobile menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,20 @@
 import React from "react"
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 import MainNav from "../components/mainnav.js"
 
 export default function Header() {
     return (
-        <header className="container-fluid position-absolute bg-gray-gradient-70-to-0 top-0">
-            <div className="row ml-4 mr-4">
-                    <div className="col-sm-12 col-lg-4 d-flex justify-content-center justify-content-lg-start align-items-center p-4">
-                        {/* <img src={logo} className="logo" alt="williamshelly.dev Restaurant" width="298" height="137" /> */}
-                    </div>
-                    <div className="col-sm-12 col-lg-8 px-4 py-2 d-flex justify-content-center justify-content-lg-end align-items-center">
-                        <MainNav></MainNav>
-                    </div>
-                </div>
-        </header>
+		<header>
+			<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+				<Navbar.Brand href="/">WS</Navbar.Brand>
+				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Nav className="mr-auto">
+						<MainNav></MainNav>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+		</header>
     )
 }
--- src/components/mainnav.js
+++ src/components/mainnav.js
@@ -3,13 +3,12 @@ import { Link } from "gatsby"
 
 export default function MainNav() {
     return (
-        <ul className="main-nav text-center text-lg-left">
-            <li><Link to="/" className="">Home</Link></li>
-            <li><Link to="/about" className="">About</Link></li>
-            <li><a href="/portfolio" className="">Portfolio</a></li>
-            <li><a href="/cheat-sheets" className="">Cheat Sheets</a></li>
-            <li><a href="/links" className="">Links</a></li>
-            <li><a href="/contact" className="">Contact</a></li>
-        </ul>
+			<ul className="main-nav text-lg-left">
+				<li><Link to="/about" className="">About</Link></li>
+				<li><Link to="/portfolio" className="">Portfolio</Link></li>
+				<li><Link to="/cheat-sheets" className="">Cheat Sheets</Link></li>
+				<li><Link to="/links" className="">Links</Link></li>
+				<li><Link to="/contact" className="">Contact</Link></li>
+			</ul>
     )
 }
