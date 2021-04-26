6203e1a6e352c51e770c9fa48d1887df2bf0a0ef
william-shelly <william-shelly@users.noreply.github.com>
2021-04-26 17:06:46 -0500

    Adding icons to page and additional styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,122 @@ import Homepage from '../layouts/homepage.js'
 import SEO from '../components/seo.js'
 import hero from '../images/florian-olivo-4hbJ-eymZ1o-unsplash.jpg'
 import 'bootstrap/dist/css/bootstrap.css'
-import { ReactJs, Gatsby } from '@icons-pack/react-simple-icons';
+import { ReactJs, Gatsby, Javascript, Jquery, Adobephotoshop, Adobeillustrator, Adobexd, Visualstudiocode, Bootstrap, Tailwindcss, Git, Github, Xcode, Json, Wordpress, Googlechrome, Safari, Applemusic, Applepodcasts, Googlecalendar, Netlify, Contentful, Php, CssThree, Npm, NodeDotJs, Apple } from '@icons-pack/react-simple-icons';
 import '../../styles.scss'
 
 function gatsbyIcon() {
-  return <Gatsby className="text-secondary m-4" size={100} />;
+  return <Gatsby className="text-secondary m-4 opacity-0 delay-65" />;
 }
 
 function reactIcon() {
-  return <ReactJs className="text-secondary m-4" size={100} />;
+  return <ReactJs className="text-secondary m-4 opacity-0 delay-110" />;
 }
+
+function javascriptIcon() {
+  return <Javascript className="text-secondary m-4 opacity-0 delay-15" />;
+}
+
+function jqueryIcon() {
+  return <Jquery className="text-secondary m-4 opacity-0 delay-55" />;
+}
+
+function photoshopIcon() {
+  return <Adobephotoshop className="text-secondary m-4 opacity-0 delay-60" />;
+}
+
+function illustratorIcon() {
+  return <Adobeillustrator className="text-secondary m-4 opacity-0 delay-105" />;
+}
+
+function xdIcon() {
+  return <Adobexd className="text-secondary m-4 opacity-0 delay-35" />;
+}
+
+
+function vsCodeIcon() {
+  return <Visualstudiocode className="text-secondary m-4 opacity-0 delay-40" />;
+}
+
+function bootstrapIcon() {
+  return <Bootstrap className="text-secondary m-4 opacity-0 delay-45" />;
+}
+
+function tailwindcssIcon() {
+  return <Tailwindcss className="text-secondary m-4 opacity-0 delay-130" />;
+}
+
+function gitIcon() {
+  return <Git className="text-secondary m-4 opacity-0 delay-90" />;
+}
+
+function githubIcon() {
+  return <Github className="text-secondary m-4 opacity-0 delay-115" />;
+}
+
+function xcodeIcon() {
+  return <Xcode className="text-secondary m-4 opacity-0 delay-145" />;
+}
+
+function jsonIcon() {
+  return <Json className="text-secondary m-4 opacity-0 delay-70" />;
+}
+
+function wordPressIcon() {
+  return <Wordpress className="text-secondary m-4 opacity-0 delay-125" />;
+}
+
+function googleChromeIcon() {
+  return <Googlechrome className="text-secondary m-4 opacity-0 delay-80" />;
+}
+
+function safariIcon() {
+  return <Safari className="text-secondary m-4 opacity-0 delay-140" />;
+}
+
+function appleMusicIcon() {
+  return <Applemusic className="text-secondary m-4 opacity-0 delay-120" />;
+}
+
+function applePodcasts() {
+  return <Applepodcasts className="text-secondary m-4 opacity-0 delay-100" />;
+}
+
+function googlecalendarIcon() {
+  return <Googlecalendar className="text-secondary m-4 opacity-0 delay-30" />;
+}
+
+function netlifyIcon() {
+  return <Netlify className="text-secondary m-4 opacity-0 delay-10" />;
+}
+
+function contentfulIcon() {
+  return <Contentful className="text-secondary m-4 opacity-0 delay-135" />;
+}
+
+function phpIcon() {
+  return <Php className="text-secondary m-4 opacity-0 delay-95" />;
+}
+
+function cssThreeIcon() {
+  return <CssThree className="text-secondary m-4 opacity-0 delay-05" />;
+}
+
+function npmIcon() {
+  return <Npm className="text-secondary m-4 opacity-0 delay-25" />;
+}
+
+function nodeJsIcon() {
+  return <NodeDotJs className="text-secondary m-4 opacity-0 delay-20" />;
+}
+
+function appleIcon() {
+  return <Apple className="text-secondary m-4 opacity-0 delay-50" />;
+}
+
+// function myFunction() {
+//   heroIconContainer = document.querySelector('hero-icon-container')
+//   console.log(heroIconContainer)
+// }
 
 // markup
 const IndexPage = () => {
@@ -32,10 +138,35 @@ const IndexPage = () => {
       <section className="hero">
         <div className="hero-img-container">
           <img className="hero-img" src={hero} alt={heroAlt} />
-          <div className="hero-icon-container d-flex justify-content-center align-items-center">
-            {gatsbyIcon()}
-            {reactIcon()}
-        </div>
+          <div className="hero-icon-container d-flex flex-wrap">
+            { gatsbyIcon() }
+            { reactIcon() }
+            { javascriptIcon() }
+            { jqueryIcon() }
+            { photoshopIcon() }
+            { illustratorIcon() }
+            { xdIcon() }
+            { vsCodeIcon() }
+            { bootstrapIcon() }
+            { tailwindcssIcon() }
+            { gitIcon() }
+            { githubIcon() }
+            { xcodeIcon() }
+            { jsonIcon() }
+            { wordPressIcon() }
+            { googleChromeIcon() }
+            { safariIcon() }
+            { appleMusicIcon() }
+            { applePodcasts() }
+            { googlecalendarIcon() }
+            { netlifyIcon() }
+            { contentfulIcon() }
+            { phpIcon() }
+            { cssThreeIcon() }
+            { npmIcon() }
+            { nodeJsIcon() }
+            { appleIcon() }
+          </div>
         </div>
         <div className="hero-content">
           <h1 className="fs-0 text-uppercase monofont color-primary">{site}</h1>
@@ -47,6 +178,7 @@ const IndexPage = () => {
           </div>
         </div>
       </section>
+      {/* {myFunction()} */}
     </Homepage>
   )
 }
