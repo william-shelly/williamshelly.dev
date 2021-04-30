9488f2e4ea2f66f0cc7b6b248f706e44581d2bc5
william-shelly <william-shelly@users.noreply.github.com>
2021-04-30 08:37:59 -0500

    Fixing gatsby srcSet error

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,40 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import Homepage from '../layouts/homepage.js'
-import SEO from '../components/seo.js'
-import hero from '../images/florian-olivo-4hbJ-eymZ1o-unsplash.webp'
-import heroMobile from '../images/florian-olivo-4hbJ-eymZ1o-unsplash-mobile.webp'
-import IconGallery from '../components/icon-gallery'
-import 'bootstrap/dist/css/bootstrap.css'
-import '../../styles.scss'
-import h1WS from '../images/h1-ws.svg'
-import h1Freelance from '../images/h1-freelance.svg'
+import * as React from "react";
+import { Link } from "gatsby";
+import Homepage from "../layouts/homepage.js";
+import SEO from "../components/seo.js";
+import hero from "../images/florian-olivo-4hbJ-eymZ1o-unsplash.webp";
+import heroMobile from "../images/florian-olivo-4hbJ-eymZ1o-unsplash-mobile.webp";
+import IconGallery from "../components/icon-gallery";
+import "bootstrap/dist/css/bootstrap.css";
+import "../../styles.scss";
+import h1WS from "../images/h1-ws.svg";
+import h1Freelance from "../images/h1-freelance.svg";
 
 // markup
 const IndexPage = () => {
-
-  const heroAlt = 'html code';
-  const site='williamshelly.dev';
-  const subheadline='freelance designer / front-end developer';
-  const content='Hello! My name is William.';
-  const content2='I design and create digital stuff.';
-  const photoCredit='Florian Olivo';
-  const photoLink='https://unsplash.com/photos/4hbJ-eymZ1o?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink';
-  const title = content.replace('.','')
-  const HomePageSrcSet = heroMobile + ' 480w, ' + hero + ' 800w'
+  const heroAlt = "html code";
+  const site = "williamshelly.dev";
+  const subheadline = "freelance designer / front-end developer";
+  const content = "Hello! My name is William.";
+  const content2 = "I design and create digital stuff.";
+  const photoCredit = "Florian Olivo";
+  const photoLink =
+    "https://unsplash.com/photos/4hbJ-eymZ1o?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink";
+  const title = content.replace(".", "");
+  const HomePageSrcSet = heroMobile + " 480w, " + hero + " 800w";
 
   return (
     <Homepage>
       <SEO title={title}></SEO>
       <section className="hero">
         <div className="hero-img-container">
-          <img className="hero-img"
-            srcset={HomePageSrcSet}
+          <img
+            className="hero-img"
+            srcSet={HomePageSrcSet}
             sizes="(max-width: 600px) 480px, 800px"
             src={hero}
-            alt={heroAlt} />
+            alt={heroAlt}
+          />
           <div className="hero-icon-container d-flex flex-wrap">
             <IconGallery />
           </div>
@@ -45,19 +47,36 @@ const IndexPage = () => {
           <h3 className="fs-6 text-white">{content}</h3>
           <h4 className="fs-6 text-white">{content2}</h4>
           <div className="photo-credit background-color-primary p-3">
-            <a className="text-white" href={photoLink} target="_blank" rel="noopener noreferrer">Thank you <strong>{photoCredit}</strong>! For such a wonderful photo!</a>
+            <a
+              className="text-white"
+              href={photoLink}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Thank you <strong>{photoCredit}</strong>! For such a wonderful
+              photo!
+            </a>
           </div>
         </div>
       </section>
       <div className="jumbotron text-center bg-white">
         <h2 className="fs-4 my-4 text-center color-primary">
-          Do you need help with Design Work<br />
-          or a Developer to Code your Site?</h2>
-        <p className="text-base my-4 text-center">Let's discuss your project and how I can help you meet your goals.</p>
-        <Link className="btn btn-primary d-sm-inline-block m-0-auto align-center mr-2" to="/contact">Let's Talk!</Link>
+          Do you need help with Design Work
+          <br />
+          or a Developer to Code your Site?
+        </h2>
+        <p className="text-base my-4 text-center">
+          Let's discuss your project and how I can help you meet your goals.
+        </p>
+        <Link
+          className="btn btn-primary d-sm-inline-block m-0-auto align-center mr-2"
+          to="/contact"
+        >
+          Let's Talk!
+        </Link>
       </div>
     </Homepage>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
